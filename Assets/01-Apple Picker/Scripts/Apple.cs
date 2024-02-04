a14843ef7099d44004062e1a70f1b1a7f45bfd60
MaxMicheller1 <157072693+MaxMicheller1@users.noreply.github.com>
2024-02-03 22:59:31 -0500

    Apple Deletion
    Made it so the apples get deleted when exiting the bottom of the viewable screen.

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour
-{
+public class Apple : MonoBehaviour{
+	
+	public static float bottomY = -20f;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,9 @@ public class Apple : MonoBehaviour
     void Update()
     {
         
+		if (transform.position.y < bottomY){
+			
+			Destroy( this.gameObject);
+		}
     }
 }

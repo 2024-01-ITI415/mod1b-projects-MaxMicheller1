b5da041db12a2c38caeaa9df00ceec85c04d971a
MaxMicheller1 <157072693+MaxMicheller1@users.noreply.github.com>
2024-02-18 18:28:17 -0500

    Trying to get projectiles to collide with ballons

--- Assets/03-Prototype1/Scripts/Goal1.cs
+++ Assets/03-Prototype1/Scripts/Goal1.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Goal1 : MonoBehaviour
 {
-	static public bool 	goalMet = false;
+	//static public bool 	goalMet = false;
 
 	void OnTriggerEnter(Collider other) {
 		// when the trigger is hit by something
 		// check to see if it's a Projectile 
-		if (other.gameObject.tag == "Projectile") {
+		if (other.gameObject.tag == "Projectile1") {
 			// if so, set goalMet = true
-			Goal.goalMet = true;
+			Destroy (other);
+		
+			
+			
 
-			// also set the alpha of the color of higher opacity
-			Material mat = GetComponent<Renderer>().material;
-			Color c = mat.color;
-			c.a = 1;
-			mat.color = c;
+
+
 		}
 	}
     // Start is called before the first frame update
